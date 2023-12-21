2896729bb13bc80dc783a7db74931a799bd71ff0
Dillon Fagan <dillonfagan@pm.me>
2023-12-21 10:20:00 -0500

    Wrap AppBar and AppShell components

---
+++ apps/profile/src/lib/components/AppBar.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { AppBar } from '@skeletonlabs/skeleton';
+
+  export let title: string;
+</script>
+
+<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+  <svelte:fragment slot="lead">
+    &nbsp;
+  </svelte:fragment>
+  <strong>{title}</strong>
+  <svelte:fragment slot="trail">
+    <slot name="trail" />
+  </svelte:fragment>
+</AppBar>
---
+++ apps/profile/src/lib/components/AppShell.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { AppShell } from "@skeletonlabs/skeleton";
+</script>
+
+<AppShell>
+  <svelte:fragment slot="header">
+    <slot name="header" />
+  </svelte:fragment>
+  <main class="w-full h-full p-4 flex flex-col items-center justify-center">
+    <slot />
+  </main>
+</AppShell>
